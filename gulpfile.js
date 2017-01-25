26a88d84186c3bb0e2d8948e8485f87bc54abb61
David Erickson <davidalerickson@hotmail.com>
2017-01-25 15:32:07 -0500

    Added Compass Process

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,50 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];//array so that you can add multiple coffescript files
-var jsSources =  ['components/scripts/rclick.js',
-                  'components/scripts/pixgrid.js',
-                  'components/scripts/tagline.js',
-                  'components/scripts/template.js'];
+    var env,
+        coffeeSources,
+        jsSources,
+        sassSources,
+        htmlSources,
+        jsonSources,
+        outputDir,
+        sassStyle;
 
-gulp.task('coffee', function(){
-  gulp.src(coffeeSources)
-    .pipe(coffee({bare: true})
-    .on('error', gutil.log)
-    )
-    .pipe(gulp.dest('components/scripts'))
-});
 
-gulp.task("js", function(){
-  gulp.src(jsSources)
-  .pipe(concat('script.js'))
-  .pipe(browserify())
-  .pipe(gulp.dest('builds/development/js'))
-});
+        coffeeSources = ['components/coffee/tagline.coffee'];
+        jsSources = [
+          'components/scripts/rclick.js',
+          'components/scripts/pixgrid.js',
+          'components/scripts/tagline.js',
+          'components/scripts/template.js'
+        ];
+        sassSources = ['components/sass/style.scss'];
+
+        gulp.task('coffee', function() {
+          gulp.src(coffeeSources)
+            .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+            .pipe(gulp.dest('components/scripts'))
+        });
+
+        gulp.task('js', function() {
+          gulp.src(jsSources)
+            .pipe(concat('script.js'))
+            .pipe(browserify())
+            .pipe(gulp.dest('builds/development/css'))
+        });
+
+        gulp.task('compass', function() {
+          gulp.src(sassSources)
+            .pipe(compass({
+              sass: 'components/sass',
+              image: 'builds/development/images',
+              style: 'expanded',
+              lineNumbers: true
+            })
+            .on('error', gutil.log))
+            .pipe(gulp.dest('builds/development/css'))
+        });
